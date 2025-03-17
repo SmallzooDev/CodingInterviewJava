a8e4df2bf74965a54a1430f1b982f52d3ac07af5
smallzooDev <smallzooDev@gmail.com>
2025-03-17 21:15:53 +0900

    init

--- src/problems/leetcode/numberOfIslands/Solution.java
+++ src/problems/leetcode/numberOfIslands/Solution.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class Solution {
     private final int[] dy = {1, 0, -1, 0};
     private final int[] dx = {0, 1, 0, -1};
-    private int[][] visited;
     private int yLen;
     private int xLen;
 
@@ -13,11 +12,6 @@ public class Solution {
         int result = 0;
         yLen = grid.length;
         xLen = grid[0].length;
-        visited = new int[yLen][xLen];
-
-        for (int[] row : visited) {
-            Arrays.fill(row, 0);
-        }
 
         for (int i = 0; i < yLen; i++) {
             for (int j = 0; j < xLen; j++) {
@@ -27,25 +21,20 @@ public class Solution {
                 }
             }
         }
-
         return result;
     }
 
-    public void dfs(char[][] grid, int y, int x) {
-        if (grid[y][x] == '0') {
+    private void dfs(char[][] grid, int y, int x) {
+        if (y < 0 || x < 0 || y >= yLen || x >= xLen || grid[y][x] == '0') {
             return;
         }
 
+        // 방문 표시
         grid[y][x] = '0';
+
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-
-            if (ny < 0 || nx < 0 || ny >= yLen || nx >= xLen || visited[ny][nx] == 1) {
-                continue;
-            }
-
-            visited[ny][nx] = 1;
             dfs(grid, ny, nx);
         }
     }
