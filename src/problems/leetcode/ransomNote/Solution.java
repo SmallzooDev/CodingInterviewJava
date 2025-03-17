1c8c8895aaa1ad871e27ba53e8437f8a06d4f735
smallzooDev <smallzooDev@gmail.com>
2025-03-17 20:06:10 +0900

    init

---
+++ src/problems/leetcode/ransomNote/Solution.java
@@ -0,0 +1,22 @@
+package problems.leetcode.ransomNote;
+
+import java.util.HashMap;
+
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> magazineMap = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            magazineMap.put(c, magazineMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (!magazineMap.containsKey(c) || magazineMap.get(c) == 0) {
+                return false;
+            }
+            magazineMap.put(c, magazineMap.get(c) - 1);
+        }
+
+        return true;
+    }
+}
